91097e52ebc7025cc06afe9319c9bfeeabdadb4e
Joshua Cook-Harding <joshua.cook-harding@inphysec.co.nz>
2022-11-30 21:56:17 +0000

    Testing from devbox

---
+++ src/Day1.java
@@ -0,0 +1,11 @@
+public class Day1 {
+
+    public static void part1(){
+        System.out.println("howdy");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Initialising");
+        part1();
+    }
+}
