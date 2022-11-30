6f7d78b4ee95c597ab9e2b6f54970097df01bed9
Josh <joshua.cook1203@gmail.com>
2022-11-30 18:37:54 +1300

    reads a file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void task(){
+        System.out.println("task1");
+        File inputFile = new File("input.txt");
+        try {
+        Scanner scan = new Scanner(inputFile);
+        while (scan.hasNext()){
+            String x = scan.nextLine();
+            System.out.println(x);
+        }
+        scan.close();
+    } catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+    }
+    }
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Initialising");
+        task();
     }
 }
