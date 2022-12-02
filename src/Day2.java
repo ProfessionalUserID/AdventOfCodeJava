fe51c8d05a496f03eca633f5d0631da95cae3804
Joshua Cook-Harding <joshua.cook-harding@inphysec.co.nz>
2022-12-02 08:16:18 +0000

    Day  not entirely working yet. Too low

--- src/Day2.java
+++ src/Day2.java
@@ -1,12 +1,72 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Day2 {
 
-    public static void part1(){
+    public static void part1() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("input.txt"));
+        int total = 0;
+        int rock = 1;
+        int paper = 2;
+        int scissors = 3;
+        int loss = 0;
+        int draw = 3;
+        int win = 6;
 
-    }
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
 
-    public static void main(String[] args) {
-        part1();
+            System.out.println(line);
+
+            char first = line.charAt(0);
+            char second = line.charAt(2);
+
+            char A = "A".charAt(0);
+            char B = "B".charAt(0);
+            char C = "C".charAt(0);
+            char X = "X".charAt(0);
+            char Y = "Y".charAt(0);
+            char Z = "Z".charAt(0);
+
+
+            if (first == A && second == X) {
+                total += (rock+draw);
+
+            } else if (first == A && second == Y) {
+                total += (rock+loss);
+
+            } else if (first == A && second == Z) {
+                total += (rock+win);
+
+            } else if (first == B && second == X) {
+                total += (paper+loss);
+
+            } else if (first == B && second == Y) {
+                total += (paper+draw);
+
+            } else if (first == B && second == Z) {
+                total += (paper+win);
+
+            } else if (first == C && second == X) {
+                total += (scissors+win);
+
+            } else if (first == C && second == Y) {
+                total += (scissors+loss);
+
+            } else if (first == C && second == Z) {
+                total += (scissors+draw);
+
+            } else {
+                System.out.println("Something broke oopsie woopsie");
+                break;
+            }
+
+        }
+        System.out.print(total);
     }
+        public static void main(String[] args) throws FileNotFoundException {
+            part1();
+        }
 }
