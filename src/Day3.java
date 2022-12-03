748d931ba5437e22e7c98fcda44f669ae9f82de8
Josh <joshua.cook1203@gmail.com>
2022-12-03 22:58:11 +1300

    Day 3 initialised

--- src/Day3.java
+++ src/Day3.java
@@ -1,12 +1,37 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.*;
 
 public class Day3 {
 
-    public static void part1(){
+    public static void part1() throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("input.txt"));
+        ArrayList<Character> lower = new ArrayList<>();
+        ArrayList<Character> upper = new ArrayList<>();
+
+        HashMap<Character, Integer> lowerCase = new HashMap<>();
+        HashMap<Character, Integer> upperCase = new HashMap<>();
+
+        char c;
+        int count = 0;
+
+        for (c = 'a'; c <= 'z'; ++c){
+            lowerCase.put(c, count);
+            count++;
+        }
+        for (c = 'A'; c <= 'Z'; ++c) {
+            upperCase.put(c, count);
+            count++;
+        }
+
+
+
+
 
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException{
         part1();
     }
 }
